4c2cfe0a9e7ec6c07205ed9e628f330d43c910d9
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-01-24 01:25:38 +0900

    updated my seed

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :photo, presence: true
